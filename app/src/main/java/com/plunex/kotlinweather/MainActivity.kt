54c17919735324708ae64eb63b9011fa0e59038a
Daniel <oluwabukunmi.aluko@gmail.com>
2019-08-31 11:12:40 +0100

    set the hello world text from main activity.

--- app/src/main/java/com/plunex/kotlinweather/MainActivity.kt
+++ app/src/main/java/com/plunex/kotlinweather/MainActivity.kt
@@ -2,14 +2,14 @@ package com.plunex.kotlinweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        message.text = "Hello world!"
 
     }
 }
