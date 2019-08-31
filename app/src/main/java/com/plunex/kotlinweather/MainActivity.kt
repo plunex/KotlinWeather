53a403e5776170f592593762017ae3bccafe2897
Daniel <oluwabukunmi.aluko@gmail.com>
2019-08-31 11:41:13 +0100

    explored kotlin functions.

--- app/src/main/java/com/plunex/kotlinweather/MainActivity.kt
+++ app/src/main/java/com/plunex/kotlinweather/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.plunex.kotlinweather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         message.text = "Hello world!"
 
+        toast("Hello")
     }
+
+    fun add(x: Int, y: Int): Int {
+        return x + y
+    }
+
+    fun multiply(x: Int, y: Int): Int = x * y
+
+    fun toast(message: String,
+              tag: String = MainActivity::class.java.simpleName,
+              length: Int = Toast.LENGTH_SHORT) {
+         Toast . makeText (this, "[$tag] $message", length).show()
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/plunex/kotlinweather/Person.kt
@@ -0,0 +1,8 @@
+package com.plunex.kotlinweather
+
+
+class Person(name: String, surname: String){
+    init {
+
+    }
+}
